1a698abf4fe18f4614f7211f83a6082270fe602d
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-10-30 15:12:26 +0700

    //variabel-variabel untuk mengingat nilai masuk dari keyboard

--- app/src/main/java/com/example/pertemuan5/TeksEdit.kt
+++ app/src/main/java/com/example/pertemuan5/TeksEdit.kt
@@ -1,3 +1,18 @@
 package com.example.pertemuan5
 
 
+
+
+
+
+@composable
+fun FormDataDiri(modifier: Modifier
+){
+    //variabel-variabel untuk mengingat nilai masuk dari keyboard
+    var textNama by remember { mutableStateOf(value="") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+
+
+}
+
